1d10a7830a5698817c436e718dbeea5d5d1f233a
leemkro <leemkro@naver.com>
2025-01-22 08:39:44 +0900

    pinia 권장 문법에 맞도록 수정 ref, computed, function 으로 수정

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,17 +1,14 @@
 import { defineStore } from 'pinia';
+import { ref, computed } from 'vue';
 
-export const useCounterStore = defineStore('counter', {
-    state: () => ({
-        count: 0,
-    }),
+export const useCounterStore = defineStore('counter', () => {
+    const count = ref(0);
 
-    getters: {
-        doubleCount: (state) => state.count * 2,
-    },
+    const doubleCount = computed(() => count.value * 2);
 
-    actions: {
-        increment() {
-            this.count++;
-        },
-    },
+    function increment() {
+        count.value++;
+    }
+
+    return { count, doubleCount, increment };
 });
